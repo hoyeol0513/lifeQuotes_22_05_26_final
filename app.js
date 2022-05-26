bbff5a0b9173800a7b855e99175b7bd5dad55dac
hoyeol0513 <hoyeol0513@naver.com>
2022-05-26 11:40:07 +0900

    수정 API 구현

--- app.js
+++ app.js
@@ -54,6 +54,52 @@ app.get("/lifequotes/random", async (req, res) => {
   });
 });
 
+app.patch("/lifequotes/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const [[LifequotesRow]] = await pool.query(
+    `
+      SELECT * FROM lifequotes WHERE id = ?
+      `,
+    [id]
+  );
+  if (LifequotesRow === undefined) {
+    res.status(404).json({
+      resultCode: "F-1",
+      msg: "404 not found",
+    });
+    return;
+  }
+  const {
+    reg_Date = LifequotesRow.reg_Date,
+    content = LifequotesRow.content,
+    author = LifequotesRow.author,
+    hit_Count = LifequotesRow.hit_Count,
+    like_Count = LifequotesRow.like_Count,
+    dislike_Count = LifequotesRow.dislike_Count,
+  } = req.body;
+
+  await pool.query(
+    `
+      UPDATE lifequotes
+      SET reg_Date = ?,
+      content = ?,
+      author = ?,  
+      hit_Count = ?,
+      like_Count = ?, 
+      dislike_Count =?
+      WHERE id = ?
+      `,
+    [reg_Date, content, author, hit_Count, like_Count, dislike_Count, id]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "Success!",
+    data: LifequotesRow,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Lifequotes app listening on port ${port}`);
 });
